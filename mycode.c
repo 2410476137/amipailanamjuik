a9da3df4a123d4b07769c76d402e2ac9ec5acd9b
khademul <khademul@gmail.com>
2025-10-20 01:46:51 +0600

    Renamed variables for clarity

--- mycode.c
+++ mycode.c
@@ -2,40 +2,39 @@
 #include <stdio.h>
 int main()
 {
-    int a[3][3] = {{50, 60, 70}, {80, 90, 100}, {30, 40, 50}};
-    char
-        n[3][10] = {"Ali", "Bob", "Cat"};
-    int i, j, s;
-    float avg;
+    int student_marks[3][3] = {{50, 60, 70}, {80, 90, 100}, {30, 40, 50}};
+    char students_name[3][1] = {"Ali", "Bob", "Cat"};
+    int i, j, sum_marks;
+    float average;
     for (i = 0; i < 3; i++)
     {
-        s = 0;
+        sum_marks = 0;
         for (j = 0; j < 3; j++)
         {
-            s += a[i][j];
+            sum_marks += student_marks[i][j];
         }
-        avg = s / 3.0;
-        if (avg >= 80)
-            printf("%s Grade A avg=%.2f\n", n[i], avg);
-        else if (avg >= 60)
-           printf("%s Grade B avg=%.2f\n",n[i],avg);
-        else if(avg>=40)
-           printf("%s Grade C avg=%.2f\n",n[i],avg);
+        average = sum_marks / 3.0;
+        if (average >= 80)
+            printf("%s Grade A avg=%.2f\n", students_name[i], average);
+        else if (average >= 60)
+           printf("%s Grade B avg=%.2f\n",students_name[i],average);
+        else if(average>=40)
+           printf("%s Grade C avg=%.2f\n",students_name[i],average);
         else 
-           printf("%s Fail avg=%.2f\n",n[i],avg);
+           printf("%s Fail avg=%.2f\n",students_name[i],average);
         }
-        int max=0,pos=0;for(i=0;i<3;i++){
-                s = 0;
+        int maximam=0,position=0;for(i=0;i<3;i++){
+                sum_marks = 0;
                 for (j = 0; j < 3; j++)
                 {
-                    s += a[i][j];
+                    sum_marks += student_marks[i][j];
                 }
-                if (s > max)
+                if (sum_marks > maximam)
                 {
-                    max = s;
-                    pos = i;
+                    maximam = sum_marks;
+                    position = i;
                 }
         }
-        printf("Topper: %s with total %d\n",n[pos],max);
+        printf("Topper: %s with total %d\n",students_name[position],maximam);
         return 0;
     }
